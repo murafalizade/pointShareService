ac974f893d23ed4caed94b9392d6fbc148cbf008
muradeliyev <muradaliyev2229@gmail.com>
2024-10-14 23:02:43 +0400

    fix path

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api-docs', serve, setup(swaggerSpec));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://point-share.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
